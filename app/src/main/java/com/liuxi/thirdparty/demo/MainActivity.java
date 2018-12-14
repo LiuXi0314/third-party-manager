352ff7b3e57392babf0731e4b1de2d2659013895
LiuXi0314 <xiaoxixizhizhi@gmail.com>
2018-12-14 15:09:27 +0800

    fix package name error

---
+++ app/src/main/java/com/liuxi/thirdparty/demo/MainActivity.java
@@ -0,0 +1,18 @@
+package com.liuxi.thirdparty.demo;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * @Author liuxi
+ * @Email xiaoxixizhizhi@gmail.com
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
